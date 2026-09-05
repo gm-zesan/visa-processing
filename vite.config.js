803cab061f22a2884c6bbf42a756e41b0917a33c
gm-zesan <gmzesan7767@gmail.com>
2026-09-05 22:55:25 +0600

    Update Vite configuration for server settings and clean up commented SCSS imports

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    server: {
+        host: "127.0.0.1",
+        port: 3000,
+        hmr: {
+            host: "127.0.0.1",
+        },
+    },
     plugins: [
         laravel({
             input: [
@@ -17,14 +24,14 @@ export default defineConfig({
                 "resources/scss/frontend/our_service.scss",
                 "resources/scss/frontend/contact.scss",
                 "resources/scss/frontend/faq.scss",
-                // 'resources/scss/frontend/single_list.scss',
+                // "resources/scss/frontend/single_list.scss",
                 "resources/scss/frontend/single_blog.scss",
                 "resources/scss/frontend/country.scss",
                 "resources/scss/frontend/visa.scss",
                 "resources/scss/frontend/blog_list.scss",
-                // 'resources/scss/frontend/privacy.scss',
-                // 'resources/scss/frontend/termsofuse.scss',
-                // 'resources/scss/frontend/cookie.scss',
+                // "resources/scss/frontend/privacy.scss",
+                // "resources/scss/frontend/termsofuse.scss",
+                // "resources/scss/frontend/cookie.scss",
                 "resources/scss/frontend/helpcenter.scss",
                 "resources/scss/frontend/responsive.scss",
                 "resources/scss/frontend/helper.scss",
